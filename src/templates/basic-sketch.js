6f7cd3de1e8673061e3025382e1da40f571e2ed3
mmansion <mikhail@tellart.com>
2014-03-15 21:31:24 -0400

    Updating basic-sketch template

--- src/templates/basic-sketch.js
+++ src/templates/basic-sketch.js
@@ -1,53 +1,21 @@
-require([
-  'jquery',
-  'underscore', 
-  'classes/DrawEngine2', 
-  'classes/Utils'
-  ], 
+var c;
 
-function($, _, DrawEngine, utils) {
-  
-  window.utils = utils;
+/* Setup
+  --------------------------------------------------- */
+kitt.setup = function() {
+  c = kitt.canvas.getContext('2d');
 
-  /* Sketch Vars
-    --------------------------------------------------- */
+}
 
-  var 
+/* Update
+  --------------------------------------------------- */
+kitt.update = function() {
 
-  canvas  = document.getElementById('sketch'),
-  sketch  = new DrawEngine(canvas),
-  c       = canvas.getContext('2d'),
+}
 
-  lastRun   = + new Date,
-  delay     = 100;
 
-  /* Setup
-    --------------------------------------------------- */
+/* Draw
+  --------------------------------------------------- */
+kitt.draw = function() {
 
-  sketch.setup = function() {
-
-    canvas.width  = $(window).width();
-    canvas.height = $(window).height();
-
-  };
-
-  /* Update
-    --------------------------------------------------- */
-  sketch.update = function() {
-
-  };
-
-  /* Draw (main loop)
-    --------------------------------------------------- */
-  sketch.draw = function(time) {
-    
-    
-    if( + new Date - lastRun > delay) {
-      lastRun = + new Date;      
-    }
-
-  };
-
-  sketch.start();
-
-});
+}

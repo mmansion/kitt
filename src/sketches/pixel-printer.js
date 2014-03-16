a96dc1ac6129f658855e9b15850b76175dfd5f13
mmansion <mikhail@tellart.com>
2014-03-15 22:57:05 -0400

    Updating pixel printer example

--- src/sketches/pixel-printer.js
+++ src/sketches/pixel-printer.js
@@ -1,103 +1,91 @@
-require(['jquery', 'classes/Coords'], function($, Coords) {
+var c
+  , img;
 
-  var canvas  = document.getElementById('sketch'),
-      ctx     = canvas.getContext('2d'),
-      coords  = new Coords(),
-      image   = new Image();
+/* Setup
+  --------------------------------------------------- */
+kitt.setup = function() {
+  c = kitt.canvas.getContext('2d');
 
+  img =new Image();
+  img.onload = drawImage;
+  img.src = 'sketches/data/penguin.jpeg';
+}
 
-    canvas.width = $(window).width();
-    canvas.height = $(window).height();
-    window.resize = function() {
-      canvas.width = $(window).width();
-      canvas.height = $(window).height();
-    }
+/*
+The drawImage() method draws an image, referred to as the source image (s), 
+into a canvas, referred to as the destination canvas (d). 
 
+The drawImage() method can take three different argument sets: 
 
-    image.src = 'sketches/data/penguin.jpeg';
+drawImage( image, dx, dy) 
+drawImage( image, dx, dy, dw, dh) 
+drawImage( image, sx, sy, sw, sh, dx, dy, dw, dh)
+*/
 
-    /*
+function drawImage() {
+  // d =>  destination canvas | s = > source image
 
-    Pixel Manipulation
+  var imageData = {},
+      dx = kitt.canvas.width/2 - 150, //centering
+      dy = kitt.canvas.height/2 - 226, 
 
-    exploring the drawImage() method
+      dw = 300,  //translated size to destination canvas
+      dh = 452, 
 
-    The drawImage() method draws an image, referred to as the source image (s), 
-    into a canvas, referred to as the destination canvas (d). 
+      sx = 0, 
+      sy = 0,
 
-    The drawImage() method can take three different argument sets: 
+      //full size of original image 
+      sw = 1500, 
+      sh = 2258;
+
+    //argument options, uncomment to test them out
     
-    drawImage( image, dx, dy) 
-    drawImage( image, dx, dy, dw, dh) 
-    drawImage( image, sx, sy, sw, sh, dx, dy, dw, dh)
+    //c.drawImage(image,  dx, dy);
 
-    */
+    //c.drawImage(image, dx,  dy, dw, dh);
 
-    image.onload = function(e) {
-      // d =>  destination canvas | s = > source image
+    c.drawImage(img,  sx, sy, sw, sh, dx, dy, dw, dh);
 
-      var imageData = {},
-          dx = canvas.width/2 - 150, //centering
-          dy = canvas.height/2 - 226, 
+    imageData = c.getImageData(dx, dy, dw, dh);
 
-          dw = 300,  //translated size to destination canvas
-          dh = 452, 
+    c.clearRect(0, 0, kitt.canvas.width, kitt.canvas.height);
 
-          sx = 0, 
-          sy = 0,
+    //c.putImageData(imageData, dx, dy);
 
-          //full size of original image 
-          sw = 1500, 
-          sh = 2258;
+    var pixel = 0
+      , pixelX = dx
+      , pixelY = dy;
 
-      //argument options, uncomment to test them out
+
+    //ref: http://stackoverflow.com/questions/4899799/whats-the-best-way-to-set-a-single-pixel-in-an-html5-canvas
+
+    var id = c.createImageData(1,1)
+      , d = id.data;
+
+    var pixelPrinter = setInterval(function() {
+
+      if(pixel < imageData.data.length) {
+
+        d[0] = imageData.data[pixel];
+        d[1] = imageData.data[pixel+1];
+        d[2] = imageData.data[pixel+2];
+        d[3] = imageData.data[pixel+3];
+
+        c.putImageData(id, pixelX, pixelY);
+
+        pixel += 4;
+        pixelX++;
       
-      //ctx.drawImage(image,  dx, dy);
+      if(pixelX >= dx + dw) {
+        pixelX = dx;
+        pixelY++;
+      }
 
-      //ctx.drawImage(image, dx,  dy, dw, dh);
-
-      ctx.drawImage(image,  sx, sy, sw, sh, dx, dy, dw, dh);
-
-      imageData = ctx.getImageData(dx, dy, dw, dh);
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      //ctx.putImageData(imageData, dx, dy);
-
-      var pixel  = 0, pixelX = dx, pixelY = dy;
-
-      //ref: http://stackoverflow.com/questions/4899799/whats-the-best-way-to-set-a-single-pixel-in-an-html5-canvas
-
-      var id = ctx.createImageData(1,1),
-          d = id.data;
-
-      var pixelPrinter = setInterval(function() {
-
-        if(pixel < imageData.data.length) {
-
-          
-          d[0] = imageData.data[pixel];
-          d[1] = imageData.data[pixel+1];
-          d[2] = imageData.data[pixel+2];
-          d[3] = imageData.data[pixel+3];
-
-          ctx.putImageData(id, pixelX, pixelY);
-
-          pixel += 4;
-          pixelX++;
-          
-          if(pixelX >= dx + dw) {
-            pixelX = dx;
-            pixelY++;
-          }
-
-        } else {
-          clearInterval(pixelPrinter);
-        }
-
-      }, 0);
-
+    } else {
+      clearInterval(pixelPrinter);
     }
-});
 
+  }, 0);
+}
 

910d9b3945e934d7442f5117d226a7b891a5f858
mmansion <mikhail@tellart.com>
2014-05-22 21:27:17 -0400

    make osc-min a package.json dependency

--- src/server/dev/simple-osc-receive.js
+++ src/server/dev/simple-osc-receive.js
@@ -3,6 +3,12 @@ var osc   = require('osc-min');
 
 console.log(osc);
 
+/*
+An OSC Address Pattern is an OSC-string beginning with the character '/' (forward slash).
+An OSC message has the following general format:
+<address pattern>   <type tag string>   <arguments>
+*/
+
 var sock = udp.createSocket("udp4", function(msg, rinfo) {
   var error;
   try {

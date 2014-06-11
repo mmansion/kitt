007823c388d85a3070375649b8c6ee44d92f3ece
mmansion <mikhail@tellart.com>
2014-06-10 22:15:22 -0400

    begin migrating server app to nexus core

--- src/server/index.js
+++ src/server/index.js
@@ -1,103 +1,59 @@
-var fs   = require('fs'),
-    http = require('http'),
-    url  = require('url'),
-    path = require('path');
+/**
+ * Module dependencies.
+ */
 
-var indexPage, movie_webm, movie_mp4, movie_ogg;
+var express = require('express')
+  , app     = express()
+  , routes  = require('./routes')
+  , server  = require('http').createServer(app)
+  , path    = require('path')
+  , udp     = require('dgram').createSocket('udp4', onRequest_OSC)
+  , osc     = require('osc-min')
+  , io      = require('socket.io').listen(server)
+  , fs      = require('fs');
 
-module.exports = { 
 
-  start: function(root) {
+udp.bind("9999");
 
-    http.createServer(function (req, res) {
+// all environments
+app.set('port', process.env.PORT || 3333);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-      console.log(req);
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-      var reqResource = url.parse(req.url).pathname;
-      //console.log("Resource: " + reqResource);
+app.get('/', routes.index);
 
-      if(reqResource == "/sandbox/video-server-test.html"){
+server.listen(app.get('port'), function(){
+  console.log('http server listening on port ' + app.get('port'));
+});
 
-        fs.readFile(path.resolve(root, "sandbox/video-server-test.html"), function (err, data) {
-
-          console.log("loaded resource");
-          if (err) {
-              throw err;
-          }
-          indexPage = data;    
-        //console.log(req.headers)
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(indexPage);
-        res.end();
-      });
-      
-
-      } else if (reqResource == "/favicon.ico"){
-      
-        res.writeHead(404);
-        res.end();
-      
-      } else {
-
-        console.log("herhlskjdf;laksjdf;lkasjdf;lkasjdf;laskj");
-
-        var total;
-        
-      
-        if(reqResource == "/spaceships.webm") {
-
-          fs.readFile(path.resolve(root, "sandbox/spaceships.webm"), function (err, data) {
-
-          if (err) throw err;
-          movie_webm = data;
-
-          total = movie_webm.length;
-
-          console.log(total);
-
-          var range = req.headers.range;
-
-          console.log(range);
-          return;
-          var positions = range.replace(/bytes=/, "").split("-");
-          var start = parseInt(positions[0], 10);
-
-          // if last byte position is not present then it is the last byte of the video file.
-          var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-          var chunksize = (end-start)+1;
-
-         if(reqResource == "/spaceships.webm") {
-              res.writeHead(206, { "Content-Range": "bytes " + start + "-" + end + "/" + total, 
-                                   "Accept-Ranges": "bytes",
-                                   "Content-Length": chunksize,
-                                   "Content-Type":"video/webm"});
-              res.end(movie_webm.slice(start, end+1), "binary");
-
-            }
-
-          });
-        } 
-            
-      }
-    }).listen(9999); 
-        
+function onRequest_OSC(msg, rinfo) {
+  var error;
+  try {
+    _socketIo.emit('pedalEvent', osc.fromBuffer(msg));
+    return console.log(osc.fromBuffer(msg));
+  } catch (_error) {
+    error = _error;
+    return console.log("invalid OSC packet");
   }
 }
 
+io.sockets.on('connection', function (socket) {
+  _socketIo = socket;
 
-
-
-
-// fs.readFile(path.resolve(__dirname,"movie.mp4"), function (err, data) {
-//     if (err) {
-//         throw err;
-//     }
-//     movie_mp4 = data;
-// });
-// fs.readFile(path.resolve(__dirname,"movie.ogg"), function (err, data) {
-//     if (err) {
-//         throw err;
-//     }
-//     movie_ogg = data;
-// });
-
+  socket.emit('connected', 'connected');
+  socket.on('browserEvent', function (data) {
+    //message from the browser
+  });
+});

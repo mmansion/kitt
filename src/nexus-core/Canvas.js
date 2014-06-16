910586b905ef8fc3064bebbe73d10fdbcc96d377
mmansion <mikhail@tellart.com>
2014-06-16 09:10:19 -0400

    add canvas class

--- src/nexus-core/Canvas.js
+++ src/nexus-core/Canvas.js
@@ -5,22 +5,32 @@ define(function () {
 
   var Canvas = function() {
     var self = this;
+    
+    //public properties
+
+    self.width;
+    self.height;
+    self.context2D;
   };
 
   /* CANVAS PROTOTYPE
    --------------------------------------------------- */
 
   Canvas.prototype = {
-    
+
+    background: function(color) {
+
+      this.context2D.save(); //save the context on a stack
+
+      c.fillStyle = '#011722'; //TODO: make configurable
+      c.fillRect(0, 0, nexus.canvas.width, nexus.canvas.height);  // now fill the canvas
+
+      this.context2D.restore();
+
+    }
+  
   
   };
-
-   /* PRIVATE FUNCTIONS
-   --------------------------------------------------- */
-
-   var getTimeNow = function () {
-      return new Date().getTime();
-   };
   
   return new Canvas();
 });

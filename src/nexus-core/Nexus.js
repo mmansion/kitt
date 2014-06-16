824caa09a8fd72756c373596c2d1cf4fbff5f647
mmansion <mikhail@tellart.com>
2014-06-16 09:26:07 -0400

    load the context methods onto the canvas class prototype

--- src/nexus-core/Nexus.js
+++ src/nexus-core/Nexus.js
@@ -28,11 +28,24 @@ define([
 
       self.start = function(canvasElement) { //entry point for application
 
+
         self.canvas.width  = window.innerWidth;
         self.canvas.height = window.innerHeight;
 
         self.canvas.element   = canvasElement;
+
+
         self.canvas.context2D = canvasElement.getContext('2d');
+
+        //TODO:  move this?
+        var canvasProto = Object.getPrototypeOf(self.canvas);
+
+        //move context methods to canvas class of nexus lib
+        for(method in self.canvas.context2D) {
+          canvasProto[method] = self.canvas.context2D[method];
+        }
+
+        console.log(self.canvas);
 
         self.engine.start(canvasElement);
       }

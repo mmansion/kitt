92f3960f942051fe8f64e9ba8d53e7ac5796d148
mmansion <mikhail@tellart.com>
2014-06-16 09:15:24 -0400

    beginnings of grid

---
+++ src/nexus-core/Grid.js
@@ -0,0 +1,34 @@
+define(function () {
+
+  /* PRIVATE VARS
+   --------------------------------------------------- */
+
+  var _private = "private variable";
+
+  /* MY CLASS
+   --------------------------------------------------- */
+
+  var Grid = function() {
+    //properties, getters, setters
+
+  };
+
+  /* MY CLASS PROTOTYPE
+   --------------------------------------------------- */
+
+  Grid.prototype = {
+    
+    protoMethod: function() {
+      //prototype method
+    }
+  };
+
+  /* PRIVATE FUNCTIONS
+   --------------------------------------------------- */
+  
+  function _privateFunction() {
+
+  }
+
+  return new Grid();
+});

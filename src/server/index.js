2e4bd0671fe75b4b07a64eaaebc108db0db0d745
mmansion <mikhail@tellart.com>
2014-06-26 22:45:43 -0400

    New router pattern

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 var express = require('express')
   , osc     = require('osc-min')
   , path    = require('path')
-  , routes  = require('./../routes')
   , app     = express()
   , server  = require('http').createServer(app)
   , udp     = require('dgram').createSocket('udp4', onRequest_OSC)
@@ -18,12 +17,12 @@ module.exports = {
     app.set('port',  process.env.PORT || 3333);
     app.set('views', path.join(root, 'views'));
     app.set('view engine', 'jade');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(express.methodOverride());
-    app.use(app.router);
+    
+    //app.use(express.favicon());
+    //app.use(express.logger('dev'));
+    //app.use(express.json());
+    //app.use(express.urlencoded());
+    //app.use(express.methodOverride());
 
     //static routes
     app.use(express.static(path.join(root, '/')));
@@ -32,43 +31,41 @@ module.exports = {
     app.use(express.static(path.join(root, 'public')));
     app.use(express.static(path.join(root, '/../bower_components')));
 
-    // development only
-    if ('development' == app.get('env')) {
-      app.use(express.errorHandler());
-    }
-
-    //routes
-    app.get('/', routes.index);
 
     app.get('/sketch', function(req, res) {
       res.render('sketch', {title: 'nexus 001'});
     });
 
+
+    // apply the routes to our application
+    app.use('/', require('./router'));
+
     server.listen(app.get('port'), function(){
       console.log('http server listening on port ' + app.get('port'));
     });
   }
+
 };
 
 function onRequest_OSC(msg, rinfo) {
   var error;
 
   try {
-      _socketIo.emit('pedalEvent', osc.fromBuffer(msg));
-      return console.log(osc.fromBuffer(msg));
-    } catch (_error) {
-      error = _error;
-      return console.log("invalid OSC packet");
-    }
+    _socketIo.emit('pedalEvent', osc.fromBuffer(msg));
+    return console.log(osc.fromBuffer(msg));
+  } catch (_error) {
+    error = _error;
+    return console.log("invalid OSC packet");
   }
+}
 
-  io.sockets.on('connection', function (socket) {
-    _socketIo = socket;
+io.sockets.on('connection', function (socket) {
+  _socketIo = socket;
 
-    socket.emit('connected', 'connected');
-    socket.on('browserEvent', function (data) {
-      //message from the browser
+  socket.emit('connected', 'connected');
+  
+  socket.on('browserEvent', function (data) {
+    //message from the browser
   });
+
 });
-
-
---
+++ src/server/router.js
@@ -0,0 +1,29 @@
+/*
+ * Nexus App Routes
+ */
+
+var express = require('express')
+  , router  = new express.Router();
+ 
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+
+  // log each request to the console
+  console.log(req.method, req.url);
+
+  // continue doing what we were doing and go to the route
+  next(); 
+});
+
+// home page route (http://localhost:8080)
+router.get('/', function(req, res) {
+  res.render('index', {title: 'nexus 001'});
+});
+
+// about page route (http://localhost:8080/about)
+router.get('/about', function(req, res) {
+  res.send('im the about page!'); 
+});
+
+module.exports = router;

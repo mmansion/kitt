713b65dd6fd376e5515f59dbda2dcaf4c09dd3e1
mmansion <mikhail@tellart.com>
2014-07-01 08:42:18 -0400

    Updating Shape class with new prototype methods

--- src/cyto-core/cyShape.js
+++ src/cyto-core/cyShape.js
@@ -1,25 +1,69 @@
 define(['/cyDrawingObject.js'], function (DrawingObject) {
 
-
-  var cyShape = function(root) {
-
-    this.root = root;
-
+  var Shape = function() {
     this.drawCenter = false;
-
-    this.inherits = Object.create(DrawingObject.prototype);
-
-    //console.log(this.inherits);
   };
 
-  cyShape.prototype = {
+  var p = Shape.prototype = new DrawingObject();
 
-    drawCenter: function(bool) {
-      console.log(drawCenter);
-      this.drawCenter = bool || true;
-    }
+  p.drawCenter = function(bool) {
+    this.drawCenter = bool || true;
+  };
     
+  p.collidesWith = function(shape) {
+      var axes = this.getAxes().concat(shape.getAxes());
+      return !this.separationOnAxes(axes, shape);
+  },
+
+  p.separationOnAxes = function(axes, shape) {
+      for (var i=0; i < axes.length; ++i) {
+         axis = axes[i];
+         projection1 = shape.project(axis);
+         projection2 = this.project(axis);
+
+         if (! projection1.overlaps(projection2)) {
+            return true; // don't have to test remaining axes
+         }
+      }
+    return false;
   };
 
-  return cyShape;
+  p.move = function(dx, dy) {
+    throw 'move(dx, dy) not implemented';
+  };
+
+  p.createPath = function(context) {
+  throw 'createPath(context) not implemented';
+  };
+
+  p.getAxes = function() {
+    throw 'getAxes() not implemented';
+  };
+
+  p.project = function(axis) {
+    throw 'project(axis) not implemented';
+  };
+
+  p.fill = function(context) {
+    context.save();
+    context.fillStyle = this.fillStyle;
+    this.createPath(context);
+    context.fill();
+    context.restore();
+  };
+
+  p.stroke = function(context) {
+    context.save();
+    context.strokeStyle = this.strokeStyle;
+    this.createPath(context);
+    context.stroke();
+    context.restore();
+  };
+
+  p.isPointInPath = function(context, x, y) {
+  this.createPath(context);
+  return context.isPointInPath(x, y);
+  };
+
+  return Shape;
 });

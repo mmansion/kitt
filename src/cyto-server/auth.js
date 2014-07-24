cece8c87fd6d39e4e925c40f3b842a32ac892c6b
mmansion <mikhail@tellart.com>
2014-07-23 23:27:48 -0400

    Building CouchDB user schema

--- src/cyto-server/auth.js
+++ src/cyto-server/auth.js
@@ -2,11 +2,51 @@
  * Cyto User Authentication
  */
 
-var express = require('express')
-  , auth    = new express.Router()
-  , nano    = require('nano')('http://localhost:5984/') 
-  , cytodb  = nano.use('cytodb')
-  ;
+var express       = require('express')
+  , auth          = new express.Router()
+  , nano          = require('nano')('http://localhost:5984/') 
+  , cytodb        = nano.use('cytodb')
+  , passport      = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+
+passport.use(new LocalStrategy(
+
+  function(username, password, done) {
+
+    console.log(username);
+    console.log(password);
+
+    // var data = req.body;
+
+    // cytodb.get('users', function(err, doc) {
+    //   if(!err) {
+    //     if(doc.hasOwnProperty(data.username)) {
+    //       if(doc[data.username].password === data.password) {
+    //         res.json({ status: '200', message: 'username and password accepted' });
+    //       } else {
+    //         res.json({ status: '401', message: 'invalid password' });
+    //       }
+    //     } else {
+    //       res.json({ status: '404', message: 'username not found' });
+    //     }
+    //   } else {
+    //     res.json({ status: '505', message: 'database error' });
+    //   }
+    // });
+
+    // User.findOne({ username: username }, function(err, user) {
+    //   if (err) { return done(err); }
+    //   if (!user) {
+    //     return done(null, false, { message: 'Incorrect username.' });
+    //   }
+    //   if (!user.validPassword(password)) {
+    //     return done(null, false, { message: 'Incorrect password.' });
+    //   }
+    //   return done(null, user);
+    // });
+  }
+));
 
 /* Top-level middleware for every users request
    -------------------------------------------------- */
@@ -28,39 +68,25 @@ auth.use(function(req, res, next) {
 /* Core Views / Pages
    -------------------------------------------------- */
 
-auth.route('/')
+auth.post('/', passport.authenticate('local', { 
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true })
+);
 
-.get(function(req, res) {
 
-  res.json({ message: 'get request received' });
-})
+// auth.route('/')
 
-.post(function(req, res) {
+// .post(function(req, res) {
 
-  var data = req.body;
+  
+// });
 
-  cytodb.get('users', function(err, doc) {
-    if(!err) {
-      if(doc.hasOwnProperty(data.username)) {
-        if(doc[data.username].password === data.password) {
-          res.json({ status: '200', message: 'username and password accepted' });
-        } else {
-          res.json({ status: '401', message: 'invalid password' });
-        }
-      } else {
-        res.json({ status: '404', message: 'username not found' });
-      }
-    } else {
-      res.json({ status: '505', message: 'database error' });
-    }
-  });
-});
-
-function errorHandler(err) {
-  if(err) {
-    console.log('error:', err.status_code, err.request.uri);
-  }
-}
+// function errorHandler(err) {
+//   if(err) {
+//     console.log('error:', err.status_code, err.request.uri);
+//   }
+// }
 
 
 module.exports = auth;
--- src/cyto-server/router.js
+++ src/cyto-server/router.js
@@ -10,6 +10,8 @@ var express = require('express')
 
 router.use(function(req, res, next) {
 
+  console.log("another router");
+
   // log each request to the console
   console.log(req.method, req.url);
 

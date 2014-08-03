20cd39d4890b6d30174f069be01f13d9e9b763ab
mmansion <mikhail@tellart.com>
2014-08-03 17:56:14 -0400

    Cleanup ng-app init

--- src/public/scripts/app2.js
+++ src/public/scripts/app2.js
@@ -2,13 +2,11 @@ define(['cyto','angular'], function(Cyto, angular) {
 
   window.cyto = {}; //global cyto namespace
 
-  //$(function() { //on document load
 
-    cyto = new Cyto(); //initialize the cyto app
+  cyto = new Cyto(); //initialize the cyto app
+
 
     //TODO: use angular to bind to cyto dom element,
     //  and change sketch when data-sketch attribute changes
-
-  //});
 
 });

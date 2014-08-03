bb13172cfc2b8561e9b50d0b21ad731cf81fb60c
mmansion <mikhail@tellart.com>
2014-08-03 14:38:06 -0400

    Move mouse events from Shape to DrawingObject class

--- src/cyto-core/cyDrawingObject.js
+++ src/cyto-core/cyDrawingObject.js
@@ -57,6 +57,33 @@ define(['/cyConstants.js', '/cyUtils.js'], function(CONSTANTS, utils) {
 
   var p = cyDrawingObject.prototype;
 
+  p._registerEvents = function (object) {
+    //hook into the root's mouse events
+    this.root.on('mouseMove', this._mouseMove.bind(this));
+    this.root.on('mouseDown', this._mouseDown.bind(this));
+    this.root.on('mouseUp',   this._mouseUp.bind(this));
+  };
+
+  p._mouseDown = function (e) {
+    if(e.x > this.left && e.x < this.right && e.y > this.top && e.y < this.bottom) {
+      if(this._hasBeenDrawn && this.draggable) this._dragging = true;
+    }
+  };
+
+  p._mouseUp = function () {
+    if(this.draggable) this._dragging = false;
+  };
+
+  p._mouseMove = function (e) {
+    if(this._dragging) {
+      this._dragging = true;
+      this.x = (this.drawCenter) ? e.x - this.width / 2 : e.x;
+      this.y = (this.drawCenter) ? e.y - this.height / 2 : e.y;
+      this.clear();
+      this.draw();
+    }
+  };
+
   p._bindToView = function() {
     var includesFilter = this.root.viewProperties
       , excludesFilter = this; //avoid collisions with this
--- src/cyto-core/cyShape.js
+++ src/cyto-core/cyShape.js
@@ -7,33 +7,6 @@ define(['/cyDrawingObject.js'], function (DrawingObject) {
   };
 
   var p = Shape.prototype = new DrawingObject();
-
-  p._registerEvents = function (object) {
-    //hook into the root's mouse events
-    this.root.on('mouseMove', this._mouseMove.bind(this));
-    this.root.on('mouseDown', this._mouseDown.bind(this));
-    this.root.on('mouseUp',   this._mouseUp.bind(this));
-  };
-
-  p._mouseDown = function (e) {
-    if(e.x > this.left && e.x < this.right && e.y > this.top && e.y < this.bottom) {
-      if(this._hasBeenDrawn && this.draggable) this._dragging = true;
-    }
-  };
-
-  p._mouseUp = function () {
-    if(this.draggable) this._dragging = false;
-  };
-
-  p._mouseMove = function (e) {
-    if(this._dragging) {
-      this._dragging = true;
-      this.x = (this.drawCenter) ? e.x - this.width / 2 : e.x;
-      this.y = (this.drawCenter) ? e.y - this.height / 2 : e.y;
-      this.clear();
-      this.draw();
-    }
-  };
     
   p.collidesWith = function(shape) {
       var axes = this.getAxes().concat(shape.getAxes());
@@ -58,7 +31,7 @@ define(['/cyDrawingObject.js'], function (DrawingObject) {
   };
 
   p.createPath = function(context) {
-  throw 'createPath(context) not implemented';
+    throw 'createPath(context) not implemented';
   };
 
   p.getAxes = function() {
@@ -70,8 +43,8 @@ define(['/cyDrawingObject.js'], function (DrawingObject) {
   };
 
   p.isPointInPath = function(context, x, y) {
-  this.createPath(context);
-  return context.isPointInPath(x, y);
+    this.createPath(context);
+    return context.isPointInPath(x, y);
   };
 
   return Shape;

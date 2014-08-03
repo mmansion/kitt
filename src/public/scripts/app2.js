2326f721ca6e319d409df39fd0c1b031a30af922
mmansion <mikhail@tellart.com>
2014-08-03 15:54:51 -0400

    Simplified initialization script "app2"

---
+++ src/public/scripts/app2.js
@@ -0,0 +1,11 @@
+define(['cyto','jquery'], function(Cyto, $, hljs) {
+
+  window.cyto = {}; //global cyto namespace
+
+  $(function() { //on document load
+
+    cyto = new Cyto(); //initialize the cyto app
+
+  });
+
+});

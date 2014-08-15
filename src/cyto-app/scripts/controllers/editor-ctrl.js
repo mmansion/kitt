fcc71885ec87240ce802d464f5d371456d5208d9
mmansion <mikhail@tellart.com>
2014-08-15 09:46:28 -0400

    Establishing pattern for ui controllers

--- src/cyto-app/scripts/controllers/editor-ctrl.js
+++ src/cyto-app/scripts/controllers/editor-ctrl.js
@@ -2,24 +2,15 @@ define(['cyEditor'], function(CyEditor) {
 
   return ['$scope', '$http', function($scope, $http) {
 
-    var cyEditor;
-
-    $scope.init = function () { //initialize controller
-
-      cyEditor = new CyEditor($scope);
-      
-    }
-
-    // You can access the scope of the controller from here
-    $scope.welcomeMessage = 'cyEditor!';
-
-    $scope.userType = 'guest';
-
+    var cyEditor = new CyEditor($scope);
    
     // Because controller is loaded asynchronously Angular's initial call
     // to $apply gets missed, and we have to explicitly call it.
 
-    $scope.$apply();
-    $scope.init();
+    $scope.$apply(function() {
+
+      //calls init on the scope, which should be defined within the cyEditor class
+      $scope.init();
+    });
   }];
 });
--- src/cyto-ui/cyEditor.js
+++ src/cyto-ui/cyEditor.js
@@ -7,27 +7,32 @@ define([
 
   var CyEditor = function($scope) {
 
+    /* CyEditor Initialization
+       -------------------------------------------------- */
+
+    $scope.init = function() {
+
+      var codeArea = document.getElementById('code')
+        , submitBtn = document.getElementById('save')
+        , codeMirro = CodeMirror
+          .fromTextArea(document.getElementById('code'), {
+          mode:  "javascript",
+          styleActiveLine: true,
+          matchBrackets: true,
+          indentUnit: 2,
+          smartIndent: true,
+          tabSize: 2,
+          indentWithTabs: false,
+          lineNumbers: true,
+          theme: 'default'
+        });
+    };
+
     $scope.submit = function() {
       console.log("a form has been submitted");
     };
 
 
-    console.log('initiating new cyEditor');
-
-    var codeArea = document.getElementById('code')
-      , submitBtn = document.getElementById('save')
-      , codeMirro = CodeMirror
-        .fromTextArea(document.getElementById('code'), {
-        mode:  "javascript",
-        styleActiveLine: true,
-        matchBrackets: true,
-        indentUnit: 2,
-        smartIndent: true,
-        tabSize: 2,
-        indentWithTabs: false,
-        lineNumbers: true,
-        theme: 'default'
-      });
   };
 
   return CyEditor;

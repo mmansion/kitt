fdf58d0210204e52cc13ee20ece55d43cae5aef0
mmansion <mikhail@tellart.com>
2014-08-15 09:55:13 -0400

    Adding template for ui controller class

---
+++ src/templates/ui-controller-class.js
@@ -0,0 +1,20 @@
+define(function () {
+
+  var CyUiClassName = function($scope) {
+
+    /* the class controller's $scope initialization
+       -------------------------------------------------- */
+
+    $scope.init = function() {
+
+ 
+    };
+
+    $scope.submit = function() {
+      
+    };
+
+  };
+
+  return CyUiClassName;
+});

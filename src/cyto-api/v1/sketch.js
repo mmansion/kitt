167ce90f07520e6d88695dcc9e20550a5082828e
mmansion <mikhail@tellart.com>
2014-08-19 08:21:03 -0400

    Differentiate methods within sketch API

--- src/cyto-api/v1/sketch.js
+++ src/cyto-api/v1/sketch.js
@@ -26,93 +26,58 @@ list
 */
 
 exports.sketch = function (req, res) {
-  api[req.params.name](req, res);
+  api[req.method.toLowerCase()][req.params.name](req, res);
 };
 
 var api = {
 
-  // list all sketches
-  list: function(req, res) {
+  /* Get API Methods
+     -------------------------------------------------- */
 
-    var respData = {};
+  get: {
 
-    //res.json({'message': 'listing sketches'});
-    db.get('sketches', function(err, doc) {
+  }
 
-      //check if _design/users view exists
-      if(err && err.status_code == 404) {
+  /* Post API Methods
+     -------------------------------------------------- */
 
-        respData = {
-          status  : 404,
-          message : 'not found',
-          data    : {} 
-        };
+  post: {
 
-      } else {
-        
-        respData = {
-          status  : 202,
-          message : 'success',
-          data    : doc.sketches
-        };
-      }
+    /**
+     * @method list
+     *
+     *  lists all sketches
+     */
 
-      res.json(respData);
+    list: function(req, res) {
 
-    });
+      var reqData  = {}
+        , respData = {};
+
+      //res.json({'message': 'listing sketches'});
+      db.get('sketches', function(err, doc) {
+
+        //check if _design/users view exists
+        if(err && err.status_code == 404) {
+
+          respData = {
+            status  : 404,
+            message : 'not found',
+            data    : {} 
+          };
+
+        } else {
+          
+          respData = {
+            status  : 202,
+            message : 'success',
+            data    : doc.sketches
+          };
+        }
+
+        res.json(respData);
+
+      });
+    }
   }
 }
-
-
-
-// api.route('/v1/sketches').get(function(req, res) {
-
-//   // var postData = {
-//   //   name: req.post.name
-//   // };
-
-//   var respData;
-
-//   db.get('sketches', function(err, doc) {
-
-//     console.log(doc.sketches.length);
-
-//     //check if _design/users view exists
-//     // if(err && err.status_code == 404) {
-
-//     //   respData = {
-//     //     status  : 404,
-//     //     message : 'not found'
-//     //   };
-
-//     // } else {
-      
-//     //   respData = {
-//     //     status  : 202,
-//     //     message : body.sketches
-//     //   }
-//     // }
-    
-//     res.json({});
-//   });
-
-//   // //add "users" view (couchdb _design doc)
-//   // db.get('_design/users', function(err, body) {
-
-//   //   //check if _design/users view exists
-//   //   if(err && err.status_code == 404) {
-
-//   //     //restore the users view from backup
-//   //     db.insert(users, '_design/users', function(err, body) {
-//   //       if (!err) onComplete(callback);
-//   //     });
-
-//   //   } else {
-//   //     onComplete(callback);
-//   //   }
-//   // });
-
-// });
-
-
-// module.exports = api;

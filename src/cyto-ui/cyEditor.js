8e93bb9d3e9f3935cf38b306df930d7da0bc58ef
mmansion <mikhail@tellart.com>
2014-09-03 00:12:34 -0400

    Resizable widget example

--- src/cyto-ui/cyEditor.js
+++ src/cyto-ui/cyEditor.js
@@ -3,9 +3,7 @@ define([
   'jquery',
   'jqueryUi',
   'cm/lib/codemirror', 
-  'cm/mode/javascript/javascript',
-  'jqUiResizable'
-
+  'cm/mode/javascript/javascript'
 
   ], function ($, jqueryUi, CodeMirror) {
 
@@ -23,11 +21,17 @@ define([
 
       console.log($('.widget-top-panel'));
       //make top panel the drag handle
-      $('#cy-editor').draggable({ handle: '.widget-top-panel' });
+      //$('#cy-editor').draggable({ handle: '.widget-top-panel' });
 
-      $('#resizable').resizable({ handles: "n, e, s, w" });
+      $("#resizable").resizable({ handles: "n, e, s, w, se, sw, nw, ne" });
+    var handles = $("#resizable").resizable("option", "handles");
+    $("#resizable").resizable("option", "handles", "n, e, s, w, se, sw, nw, ne");
 
-      console.log($('#resizable').resizable());
+     $("#resizable").find("div.ui-resizable-se").removeClass("ui-icon");
+    $("#resizable").find("div.ui-resizable-se").removeClass("ui-icon-gripsmall-diagonal-se");
+
+  
+
 
       $cyEditor.codeArea   = document.getElementById('code');
       $cyEditor.submitBtn  = document.getElementById('save');

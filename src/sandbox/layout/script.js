d5f650e81600b886444cf01c433f37c6b0b7cd68
mmansion <mikhail@tellart.com>
2014-09-24 00:42:39 -0400

    Stage 1 in multiple vertical column dynamic resizing

--- src/sandbox/layout/script.js
+++ src/sandbox/layout/script.js
@@ -1,39 +1,55 @@
 $(function() {
   console.log("loaded layout script");
 
+  function resizeAll(e,ui) {
 
-  $(".resizable-outer").resizable({ 
+    var delta = 0;
+
+    //getDirection(ui.position.left, ui.originalPosition.left);
+
+    if($(this).data('last-left')) {
+      delta =  $(this).data('last-left') - ui.position.left;
+    }
+    
+    $(this).data('last-left', ui.position.left );
+
+    if(delta !== 0) {
+
+      if(this.id === 'widgets-panel') {
+        $('.resizable').each(function(i,elem){
+          $(elem).width( $(elem).width() + (delta/3) );
+          //elem.width(delta/3);
+        });
+      }
+    }
+
+    return;
+    console.log(this.id);
+    console.log(this.style.width);
+    // var parentWidth =  
+    // var panel1Width =
+  }
+
+
+  function getDirection(left, prevLeft) {
+    console.log(left > prevLeft);
+  }
+
+  $("#widgets-panel").resizable({ 
     minWidth: 100,
     maxWidth: $(window).width() - 50,
-    handles: "w" 
+    handles: "w",
+    resize: resizeAll
   });
 
-  $("#w1").resizable({ 
-    minWidth: 50,
-    maxWidth: $(window).width() - 50,
-    containment: 'parent',
-    alsoResize: '#w2',
-    handles: "e" 
-  });
-
-  $("#w2").resizable({ 
-    minWidth: 50,
-    maxWidth: $(window).width() - 50,
-    containment: 'parent',
-    alsoResize: '#w1',
-    handles: "e" 
-  });
-
-  $("#w3").resizable({ 
-    minWidth: 50,
-    maxWidth: $(window).width() - 50,
-    containment: 'parent',
-    //alsoResize: '#w2',
-    handles: "e, w",
-    resize: function(e) {
-      console.log(e);
-    }
-  });
+  // $(".resizable").resizable({ 
+  //   minWidth: 50,
+  //   maxWidth: $(window).width() - 50,
+  //   containment: 'parent',
+  //   //alsoResize: '#w2',
+  //   handles: "e, w",
+  //   resize: resizeAll
+  // });
 
 
 });

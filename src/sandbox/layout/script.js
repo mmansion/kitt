d3da01d68a941a19d072c070a9348b5414e74dbc
mmansion <mikhail@tellart.com>
2014-10-09 00:47:35 -0400

    Multiple panel sliding interaction discovery work

--- src/sandbox/layout/script.js
+++ src/sandbox/layout/script.js
@@ -1,11 +1,77 @@
 $(function() {
   console.log("loaded layout script");
 
+  var numberVisible = 3;
+  var MAX_WIDGET_PANEL_WIDTH = 200;
+  var MIN_WIDGET_PANEL_WIDTH = 50;
+
+  var capWidthAt = false;
+
+  $("#widgets-panel").on('mousedown', function(){
+    console.log("mouse down");
+  });
+
+   $("#widgets-panel").on('mouseup', function(){
+    if(capWidthAt) capWidthAt = false;
+  });
+
   function resizeAll(e,ui) {
 
-    var delta = 0;
+    // if(capWidthAt) {
+    //   ui.size.width = capWidthAt;
+    //   return;
+    //   //$this.width(capWidthAt);
+    // }
 
-    //getDirection(ui.position.left, ui.originalPosition.left);
+    var dir = getDirection(ui.size.width, ui.prevSize.width) ? 'out' : 'in';
+
+    var id = parseInt(this.id.replace('w', ''));
+    var $this = $(this);
+    var numberToResize = numberVisible - id;
+    var deltaWidth = ui.prevSize.width - ui.size.width;
+    var lastElemNum = numberVisible;
+
+    var sumOfWidths = 0;
+    $(".resizable").each(function() {   
+      sumOfWidths += $(this).width();
+    });
+
+    if(dir == 'out') {
+
+      if(id === lastElemNum && sumOfWidths > MAX_WIDGET_PANEL_WIDTH * 3) {
+        var remainderWidth = (MAX_WIDGET_PANEL_WIDTH * 3) - (sumOfWidths - $(this).width());
+
+        $(this).width(remainderWidth);
+        return;
+
+      } else if(id < lastElemNum && $this.next().width() <= MIN_WIDGET_PANEL_WIDTH) {
+        
+          if(!capWidthAt) {
+            capWidthAt = ui.prevSize.width;
+          }
+
+          ui.size.width = capWidthAt;
+          $this.width(capWidthAt);
+          return;
+
+
+      }
+
+    } else {
+      if(capWidthAt) capWidthAt = false;
+    }
+
+    for(var i = id+1; i < id+2; i++) {
+      var $e = $('#w'+i)
+        , w  = $e.width();
+
+      $e.width(w + deltaWidth);
+    }
+
+    //getDirection(ui.size.width, ui.prevSize.width);
+
+    return;
+
 
     if($(this).data('last-left')) {
       delta =  $(this).data('last-left') - ui.position.left;
@@ -13,15 +79,15 @@ $(function() {
     
     $(this).data('last-left', ui.position.left );
 
-    if(delta !== 0) {
+    // if(delta !== 0) {
 
-      if(this.id === 'widgets-panel') {
-        $('.resizable').each(function(i,elem){
-          $(elem).width( $(elem).width() + (delta/3) );
-          //elem.width(delta/3);
-        });
-      }
-    }
+    //   if(this.id === 'widgets-panel') {
+    //     $('.resizable').each(function(i,elem){
+    //       $(elem).width( $(elem).width() + (delta/4) );
+    //       //elem.width(delta/3);
+    //     });
+    //   }
+    // }
 
     return;
     console.log(this.id);
@@ -31,25 +97,25 @@ $(function() {
   }
 
 
-  function getDirection(left, prevLeft) {
-    console.log(left > prevLeft);
+  function getDirection(currWidth, prevWidth) {
+    return currWidth > prevWidth;
   }
 
-  $("#widgets-panel").resizable({ 
-    minWidth: 100,
-    maxWidth: $(window).width() - 50,
-    handles: "w",
-    resize: resizeAll
-  });
-
-  // $(".resizable").resizable({ 
-  //   minWidth: 50,
+  // $("#widgets-panel").resizable({ 
+  //   minWidth: 100,
   //   maxWidth: $(window).width() - 50,
-  //   containment: 'parent',
-  //   //alsoResize: '#w2',
-  //   handles: "e, w",
+  //   handles: "w",
   //   resize: resizeAll
   // });
+
+  $(".resizable").resizable({ 
+    minWidth: MIN_WIDGET_PANEL_WIDTH,
+    //maxWidth: MAX_WIDGET_PANEL_WIDTH * ,
+    containment: 'parent',
+    //alsoResize: '#w2',
+    handles: "e",
+    resize: resizeAll
+  });
 
 
 });

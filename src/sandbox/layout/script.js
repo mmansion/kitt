756ab8112a179fa8a373d26cc2e2d931e6801b97
mmansion <mikhail@tellart.com>
2014-10-21 13:27:00 +0500

    Trigger mouseup event when resizable window grows over a certain amount

--- src/sandbox/layout/script.js
+++ src/sandbox/layout/script.js
@@ -1,9 +1,9 @@
 $(function() {
-  console.log("loaded layout script");
 
-  var numberVisible = 3;
-  var MAX_WIDGET_PANEL_WIDTH = 200;
-  var MIN_WIDGET_PANEL_WIDTH = 50;
+  var NUM_VISIBLE = 3
+    , MAX_WIDGET_PANEL_WIDTH = 200
+    , MIN_WIDGET_PANEL_WIDTH = 50
+    ;
 
   var capWidthAt = false;
 
@@ -17,21 +17,15 @@ $(function() {
 
   function resizeAll(e,ui) {
 
-    // if(capWidthAt) {
-    //   ui.size.width = capWidthAt;
-    //   return;
-    //   //$this.width(capWidthAt);
-    // }
+    var $this = $(this)
+      , dir = getDirection(ui.size.width, ui.prevSize.width) ? 'out' : 'in'
+      , id = parseInt(this.id.replace('w', ''))
+      , numberToResize = NUM_VISIBLE - id
+      , deltaWidth = ui.prevSize.width - ui.size.width
+      , lastElemNum = NUM_VISIBLE
+      , sumOfWidths = 0
+      ;
 
-    var dir = getDirection(ui.size.width, ui.prevSize.width) ? 'out' : 'in';
-
-    var id = parseInt(this.id.replace('w', ''));
-    var $this = $(this);
-    var numberToResize = numberVisible - id;
-    var deltaWidth = ui.prevSize.width - ui.size.width;
-    var lastElemNum = numberVisible;
-
-    var sumOfWidths = 0;
     $(".resizable").each(function() {   
       sumOfWidths += $(this).width();
     });
@@ -39,6 +33,10 @@ $(function() {
     if(dir == 'out') {
 
       if(id === lastElemNum && sumOfWidths > MAX_WIDGET_PANEL_WIDTH * 3) {
+
+        //by triggering the mouseup event we can force resize-dragging to stop
+        $this.trigger('mouseup'); 
+
         var remainderWidth = (MAX_WIDGET_PANEL_WIDTH * 3) - (sumOfWidths - $(this).width());
 
         $(this).width(remainderWidth);
@@ -46,15 +44,13 @@ $(function() {
 
       } else if(id < lastElemNum && $this.next().width() <= MIN_WIDGET_PANEL_WIDTH) {
         
-          if(!capWidthAt) {
-            capWidthAt = ui.prevSize.width;
-          }
+        if(!capWidthAt) {
+          capWidthAt = ui.prevSize.width;
+        }
 
-          ui.size.width = capWidthAt;
-          $this.width(capWidthAt);
-          return;
-
-
+        ui.size.width = capWidthAt;
+        $this.width(capWidthAt);
+        return;
       }
 
     } else {
@@ -67,50 +63,15 @@ $(function() {
 
       $e.width(w + deltaWidth);
     }
-
-    //getDirection(ui.size.width, ui.prevSize.width);
-
-    return;
-
-
-    if($(this).data('last-left')) {
-      delta =  $(this).data('last-left') - ui.position.left;
-    }
-    
-    $(this).data('last-left', ui.position.left );
-
-    // if(delta !== 0) {
-
-    //   if(this.id === 'widgets-panel') {
-    //     $('.resizable').each(function(i,elem){
-    //       $(elem).width( $(elem).width() + (delta/4) );
-    //       //elem.width(delta/3);
-    //     });
-    //   }
-    // }
-
-    return;
-    console.log(this.id);
-    console.log(this.style.width);
-    // var parentWidth =  
-    // var panel1Width =
   }
-
 
   function getDirection(currWidth, prevWidth) {
     return currWidth > prevWidth;
   }
 
-  // $("#widgets-panel").resizable({ 
-  //   minWidth: 100,
-  //   maxWidth: $(window).width() - 50,
-  //   handles: "w",
-  //   resize: resizeAll
-  // });
-
   $(".resizable").resizable({ 
     minWidth: MIN_WIDGET_PANEL_WIDTH,
-    //maxWidth: MAX_WIDGET_PANEL_WIDTH * ,
+   // maxWidth: MAX_WIDGET_PANEL_WIDTH,
     containment: 'parent',
     //alsoResize: '#w2',
     handles: "e",
